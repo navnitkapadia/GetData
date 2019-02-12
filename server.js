462706fc284ee5c8c177a7564acd8d5b81589305
navnit kapadia <navnitkapadia@gmail.com>
2019-02-12 22:31:28 +0530

    misc changes

--- server.js
+++ server.js
@@ -61,12 +61,10 @@ app.use("/api/users", users);
 app.use("/api/charts", charts);
 
 // Serve statics assets if in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
-  })
-}
+app.use(express.static('client/build'));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+})
 const port = process.env.PORT || 5000;
 
 io.listen(port, () => console.log(`Server up and running on port ${port} !`));
