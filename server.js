96493c0cae2067a3ba48fa99da147c3ad98d7a08
navnitkapadia <navnitkapadia@gmail.com>
2019-02-12 14:31:32 +0530

    A1

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use(express.static('client/build'));
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client/build','index.html'));
 })
-
+ 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
