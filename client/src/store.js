1c530f0d85088f3033ef4f48c954a2aa9540e6b5
navnitkapadia <navnitkapadia@gmail.com>
2019-02-13 10:24:56 +0530

    misc changes

--- client/src/store.js
+++ client/src/store.js
@@ -5,14 +5,13 @@ import rootReducer from "./reducers";
 const initialState = {};
 
 const middleware = [thunk];
-const devTools = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : null
 
 const store = createStore(
   rootReducer,
   initialState,
   compose(
     applyMiddleware(...middleware),
-    null
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
   )
 );
 
