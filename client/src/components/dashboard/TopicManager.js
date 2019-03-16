c68ff24107ea820b9577e348d27dc2ab172a89fa
navnit kapadia <navnitkapadia@gmail.com>
2019-03-16 20:27:57 +0530

    ASDASDASD

--- client/src/components/dashboard/TopicManager.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from "react";
-class TopicManager extends Component {
-    render(){
-        return (<div>This is topic manager</div>)
-    }
-}
-export default TopicManager;
--- client/src/config/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    soketIoUrl: 'http://localhost:3000/'
-};
-  
